fc61e468482152d9adf48a5c4ceb7010db13410e
Mohan Sai <80935489+mohansai123@users.noreply.github.com>
2023-12-04 15:31:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,12 +16,12 @@ variable "availability_zone" {
 
 variable "iam_instance_profile" {
   description = "EC2 instance IAM instance profile name"
-  default     = "pavan"
+  default     = "Mohansaiaws"
 }
 
 variable "key_name" {
   description = "EC2 instance key pair name"
-  default     = "demo-key"
+  default     = "public-cli-keypair"
 }
 
 variable "security_groups" {
@@ -32,7 +32,7 @@ variable "security_groups" {
 
 variable "subnet_id" {
   description = "The subnet ID for the EC2 instance"
-  default     = "subnet-0e41453c9b1028803"  # Update with a valid subnet ID
+  default     = "subnet-0b4304641787d445a"  # Update with a valid subnet ID
 }
 
 variable "tags" {
@@ -50,5 +50,5 @@ variable "region" {
 
 variable "s3_bucket" {
   type    = string
-  default = "pavanssonixbucket1"
+  default = "project1-s3-bucket-1"
 }
